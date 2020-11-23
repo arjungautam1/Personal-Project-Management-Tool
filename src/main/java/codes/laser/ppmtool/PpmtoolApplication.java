42daf14b7b565d009fa151c347518732f7f5ea29
arjun-sudo <arjungautam5431@gmail.com>
2020-11-23 23:06:42 +0545

    first commit

---
+++ src/main/java/codes/laser/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package codes.laser.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolApplication.class, args);
+    }
+
+}
