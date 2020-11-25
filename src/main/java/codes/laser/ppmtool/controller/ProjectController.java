236378e6fd98fc414e42a260cb07e020a99a098a
arjun-sudo <arjungautam5431@gmail.com>
2020-11-25 19:48:08 +0545

    Refactoring the Controller class

--- src/main/java/codes/laser/ppmtool/controller/ProjectController.java
+++ src/main/java/codes/laser/ppmtool/controller/ProjectController.java
@@ -7,20 +7,19 @@
 package codes.laser.ppmtool.controller;
 
 import codes.laser.ppmtool.model.Project;
+
+import codes.laser.ppmtool.services.MapValidationErrorService;
 import codes.laser.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -28,15 +27,15 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-        if (result.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-            for (FieldError error : result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+
+        ResponseEntity<?> errorMap=mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
---
+++ src/main/java/codes/laser/ppmtool/services/MapValidationErrorService.java
@@ -0,0 +1,31 @@
+/**
+ * Created By Arjun Gautam .
+ * Date: 2020-11-25
+ * Time: 19:33
+ * Project Name : ppmtool
+ */
+package codes.laser.ppmtool.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class MapValidationErrorService {
+    public ResponseEntity<?> MapValidationService(BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }
+
+}
