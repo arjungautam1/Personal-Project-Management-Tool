cdd55b3dfc714502de192a4450885ae97d752c7f
arjun-sudo <arjungautam5431@gmail.com>
2020-11-25 12:52:38 +0545

    set up object validation with @notblank and @size @column

--- src/main/java/codes/laser/ppmtool/model/Project.java
+++ src/main/java/codes/laser/ppmtool/model/Project.java
@@ -6,7 +6,11 @@
  */
 package codes.laser.ppmtool.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -15,13 +19,22 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project Name is required")
     private String projectName;
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4,max = 5,message = "Please use 4-5 characters ")
+    @Column(updatable = false,unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project Description cannot be blank ")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     public Project() {
