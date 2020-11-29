d7ab46d84aa33740d3d4b30616825db782394ee6
arjun-sudo <arjungautam5431@gmail.com>
2020-11-29 09:21:40 +0545

    import Bootstrap

--- ppmtool-client/src/App.js
+++ ppmtool-client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
     return (
         <div className="App">
