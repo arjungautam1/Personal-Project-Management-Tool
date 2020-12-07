3da0b40a09bce41e7ca5e8c564dab4c25466bafe
arjun-sudo <arjungautam5431@gmail.com>
2020-12-07 08:26:33 +0545

    Update Project form and route

--- ppmtool-client/src/components/Project/ProjectItem.js
+++ ppmtool-client/src/components/Project/ProjectItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {Link} from "react-router-dom";
 
 class ProjectItem extends Component {
   render() {
@@ -24,11 +25,11 @@ class ProjectItem extends Component {
                     </i>
                   </li>
                 </a>
-                <a href="#">
+               <Link to={`/updateProject/${project.projectIdentifier}`}>
                   <li className="list-group-item update">
                     <i className="fa fa-edit pr-lg-2"> Update Project Info</i>
                   </li>
-                </a>
+               </Link>
                 <a href="">
                   <li className="list-group-item delete">
                     <i className="fa fa-minus-circle pr-lg-5">
---
+++ ppmtool-client/src/components/Project/UpdateProject.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+
+class UpdateProject extends Component {
+    render() {
+        return (
+            <div>
+                <div className="project">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-8 m-auto">
+                                <h5 className="display-4 text-center">Update Project form</h5>
+                                <hr/>
+                                <form>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control form-control-lg "
+                                               placeholder="Project Name"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control form-control-lg"
+                                               placeholder="Unique Project ID"
+                                               disabled/>
+                                    </div>
+                                  {/*  // <!-- disabled for Edit Only!! remove "disabled" for the Create operation -->*/}
+                                    <div className="form-group">
+                                        <textarea className="form-control form-control-lg"
+                                                  placeholder="Project Description"></textarea>
+                                    </div>
+                                    <h6>Start Date</h6>
+                                    <div className="form-group">
+                                        <input type="date" className="form-control form-control-lg" name="start_date"/>
+                                    </div>
+                                    <h6>Estimated End Date</h6>
+                                    <div className="form-group">
+                                        <input type="date" className="form-control form-control-lg" name="end_date"/>
+                                    </div>
+
+                                    <input type="submit" className="btn btn-primary btn-block mt-4"/>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default UpdateProject;
