957ab3af0d23b0e72711abbde032e5b8d3e5e8f6
arjun-sudo <arjungautam5431@gmail.com>
2020-12-07 20:18:23 +0545

    Created at @Column(updatable=false)

--- src/main/java/codes/laser/ppmtool/model/Project.java
+++ src/main/java/codes/laser/ppmtool/model/Project.java
@@ -35,6 +35,7 @@ public class Project {
     private Date end_date;
 
     @JsonFormat(pattern = "yyyy-mm-dd")
+    @Column(updatable = false)
     private Date created_At;
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
