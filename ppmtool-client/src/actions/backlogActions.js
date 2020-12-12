163ed8030bd7f0517c4b68f51866a2e5e097c983
arjun-sudo <arjungautam5431@gmail.com>
2020-12-12 19:54:59 +0545

    Load project to the state

--- ppmtool-client/src/actions/backlogActions.js
+++ ppmtool-client/src/actions/backlogActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_BACKLOG } from "./types";
 
 export const addProjectTask = (backlog_id, project_task, history) => async (
   dispatch
@@ -17,4 +17,13 @@ export const addProjectTask = (backlog_id, project_task, history) => async (
       payload: e.response.data,
     });
   }
+};
+export const getBacklog = (backlog_id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/backlog/${backlog_id}`);
+    dispatch({
+      type: GET_BACKLOG,
+      payload: res.data,
+    });
+  } catch (err) {}
 };
--- ppmtool-client/src/components/ProjectBoard/ProjectBoard.js
+++ ppmtool-client/src/components/ProjectBoard/ProjectBoard.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Backlog from "./Backlog";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getBacklog } from "../../actions/backlogActions";
+import mapStateToProps from "react-redux/lib/connect/mapStateToProps";
 
 class ProjectBoard extends Component {
-  render() {
+
+
+    //constructor to handle errors
+    componentDidMount() {
+        const {id} =this.props.match.params
+        this.props.getBacklog(id);
+    }
+
+
+    render() {
     const { id } = this.props.match.params;
     return (
       <div className="container">
@@ -13,11 +26,20 @@ class ProjectBoard extends Component {
         <br />
         <hr />
         {/*Backlog STARTS HERE */}
-       <Backlog/>
+        <Backlog />
         {/*Backlog ENDS HERE */}
       </div>
     );
   }
 }
 
-export default ProjectBoard;
+ProjectBoard.propTypes = {
+  backlog: PropTypes.object.isRequired,
+  getBacklog: PropTypes.func.isRequired,
+};
+
+const mapStateToPropsProjectBoard = (state) => ({
+  backlog: state.backlog,
+});
+
+export default connect(mapStateToPropsProjectBoard, { getBacklog })(ProjectBoard);
