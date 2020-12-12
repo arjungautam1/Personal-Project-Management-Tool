5789ac03427a57e10e4937516e17efddd73386b1
arjun-sudo <arjungautam5431@gmail.com>
2020-12-12 17:12:39 +0545

    Finish AddProjectTask action , Handle errors

--- ppmtool-client/src/actions/backlogActions.js
+++ ppmtool-client/src/actions/backlogActions.js
@@ -1,8 +1,20 @@
 import axios from "axios";
+import { GET_ERRORS } from "./types";
 
 export const addProjectTask = (backlog_id, project_task, history) => async (
   dispatch
 ) => {
-  await axios.post(`/api/backlog/${backlog_id}`, project_task);
-  history.push(`/projectBoard/${backlog_id}`)
+  try {
+    await axios.post(`/api/backlog/${backlog_id}`, project_task);
+    history.push(`/projectBoard/${backlog_id}`);
+    dispatch({
+      type: GET_ERRORS,
+      payload: {},
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: e.response.data,
+    });
+  }
 };
