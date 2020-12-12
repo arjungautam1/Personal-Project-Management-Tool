97801b60f3a644f807c918e6055f0e7403cadaa8
arjun-sudo <arjungautam5431@gmail.com>
2020-12-12 13:55:21 +0545

    Routes to ProjectBoard and AddProjectTask

--- ppmtool-client/src/App.js
+++ ppmtool-client/src/App.js
@@ -2,25 +2,29 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 
 function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Header />
-          <Route exact path={"/dashboard"} component={Dashboard} />
-          <Route exact path={"/addProject"} component={AddProject} />
-          <Route exact path={"/updateProject/:id"} component={UpdateProject}/>
-        </div>
-      </Router>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <div className="App">
+                    <Header />
+                    <Route exact path={"/dashboard"} component={Dashboard}/>
+                    <Route exact path={"/addProject"} component={AddProject}/>
+                    <Route exact path={"/updateProject/:id"} component={UpdateProject}/>
+                    <Route exact path={"/projectBoard/:id"} component={ProjectBoard}/>
+                    <Route exact path={"/addProjectTask/:id"} component={AddProjectTask}/>
+                </div>
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
--- ppmtool-client/src/components/Project/ProjectItem.js
+++ ppmtool-client/src/components/Project/ProjectItem.js
@@ -1,60 +1,62 @@
-import React, {Component} from "react";
-import {Link} from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import {connect} from "react-redux";
-import {deleteProject} from "../../actions/projectActions";
+import { connect } from "react-redux";
+import { deleteProject } from "../../actions/projectActions";
 
 class ProjectItem extends Component {
-    onDeleteClick = (id) => {
-        this.props.deleteProject(id);
-    };
+  onDeleteClick = (id) => {
+    this.props.deleteProject(id);
+  };
 
-    render() {
-        const {project} = this.props;
-        return (
-            <div className="container">
-                <div className="card card-body bg-light mb-3">
-                    <div className="row">
-                        <div className="col-md-3 mr-auto border-left">
-                            <span className="text-center">{project.projectIdentifier}</span>
-                        </div>
-                        <div className="col-md-3 mr-auto">
-                            <h3>{project.projectName}</h3>
-                            <p>{project.description}</p>
-                        </div>
-                        <div className="col-md-3 ml-auto border-right">
-                            <ul className="list-group">
-                                <a href="#">
-                                    <li className="list-group-item board">
-                                        <i className="fa fa-flag-checkered pr-lg-5">Project Board</i>
-                                    </li>
-                                </a>
-                                <Link to={`/updateProject/${project.projectIdentifier}`}>
-                                    <li className="list-group-item update">
-                                        <i className="fa fa-edit pr-lg-2"> Update Project Info</i>
-                                    </li>
-                                </Link>
-
-                                <li
-                                    className="list-group-item delete"
-                                    onClick={this.onDeleteClick.bind(
-                                        this,
-                                        project.projectIdentifier
-                                    )}
-                                >
-                                    <i className="fa fa-minus-circle pr-lg-5">Delete Project</i>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    const { project } = this.props;
+    return (
+      <div className="container">
+        <div className="card card-body bg-light mb-3">
+          <div className="row">
+            <div className="col-md-3 mr-auto border-left">
+              <span className="text-center">{project.projectIdentifier}</span>
             </div>
-        );
-    }
+            <div className="col-md-3 mr-auto">
+              <h3>{project.projectName}</h3>
+              <p>{project.description}</p>
+            </div>
+            <div className="col-md-3 ml-auto border-right">
+              <ul className="list-group">
+                <Link to={`/projectBoard/${project.projectIdentifier}`}>
+                  <li className="list-group-item board">
+                    <i className="fa fa-flag-checkered pr-lg-5">
+                      Project Board
+                    </i>
+                  </li>
+                </Link>
+                <Link to={`/updateProject/${project.projectIdentifier}`}>
+                  <li className="list-group-item update">
+                    <i className="fa fa-edit pr-lg-2"> Update Project Info</i>
+                  </li>
+                </Link>
+
+                <li
+                  className="list-group-item delete"
+                  onClick={this.onDeleteClick.bind(
+                    this,
+                    project.projectIdentifier
+                  )}
+                >
+                  <i className="fa fa-minus-circle pr-lg-5">Delete Project</i>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 ProjectItem.propTypes = {
-    deleteProject: PropTypes.func.isRequired,
+  deleteProject: PropTypes.func.isRequired,
 };
 
-export default connect(null, {deleteProject})(ProjectItem);
+export default connect(null, { deleteProject })(ProjectItem);
