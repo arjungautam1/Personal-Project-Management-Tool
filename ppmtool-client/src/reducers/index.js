a2933ab3b43617c7a9019ab3a80640e7f30c8ef4
arjun-sudo <arjungautam5431@gmail.com>
2020-12-12 13:10:38 +0545

    Backlog reducer added

--- ppmtool-client/src/reducers/index.js
+++ ppmtool-client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from "redux";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
+import backlogReducer from "./backlogReducer";
 
 export default combineReducers({
     errors: errorReducer,
     project: projectReducer,
+    backlog: backlogReducer
 
 });
