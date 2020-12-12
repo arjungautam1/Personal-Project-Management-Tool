6e6fe31fbbf248b979da6b5710dc6567507caed7
arjun-sudo <arjungautam5431@gmail.com>
2020-12-12 12:22:13 +0545

    Types and Reducers for Project Tasks

--- ppmtool-client/src/reducers/index.js
+++ ppmtool-client/src/reducers/index.js
@@ -1,10 +1,9 @@
 import {combineReducers} from "redux";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
-import backlogReducer from "./backlogReducer";
 
 export default combineReducers({
     errors: errorReducer,
     project: projectReducer,
-    backlog: backlogReducer
+
 });
