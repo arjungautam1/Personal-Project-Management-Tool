ebc8d861e2a7b52e8469e69bcf7bebfe9109f7fa
arjun-sudo <arjungautam5431@gmail.com>
2020-12-13 16:26:23 +0545

    Project Update brought to the state

--- ppmtool-client/src/components/ProjectBoard/ProjectTasks/UpdateProjectTask.js
+++ ppmtool-client/src/components/ProjectBoard/ProjectTasks/UpdateProjectTask.js
@@ -5,9 +5,75 @@ import { getProjectTask } from "../../../actions/backlogActions";
 import PropTypes from "prop-types";
 
 class UpdateProjectTask extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      id: "",
+      projectSequence: "",
+      summary: "",
+      acceptanceCriteria: "",
+      status: "",
+      priority: "",
+      dueDate: "",
+      projectIdentifier: "",
+      created_At: "",
+      updated_At: "",
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
   componentDidMount() {
-    const {backlog_id,pt_id}=this.props.match.params;
+    const { backlog_id, pt_id } = this.props.match.params;
     this.props.getProjectTask(backlog_id, pt_id, this.props.history);
+  }
+  componentWillReceiveProps(nextProps, nextContext) {
+    const {
+      id,
+      projectSequence,
+      summary,
+      acceptanceCriteria,
+      status,
+      priority,
+      dueDate,
+      projectIdentifier,
+      created_At,
+      updated_At,
+    } = nextProps.project_task;
+    this.setState({
+      id,
+      projectSequence,
+      summary,
+      acceptanceCriteria,
+      status,
+      priority,
+      dueDate,
+      projectIdentifier,
+      created_At,
+      updated_At,
+    });
+  }
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    const UpdateProjectTask = {
+      id: this.state.id,
+      projectSequence: this.state.projectSequence,
+      summary: this.state.summary,
+      acceptanceCriteria: this.state.acceptanceCriteria,
+      status: this.state.status,
+      priority: this.state.priority,
+      dueDate: this.state.dueDate,
+      projectIdentifier: this.state.projectIdentifier,
+      created_At: this.state.created_At,
+      updated_At: this.state.updated_At,
+    };
+    console.log(UpdateProjectTask);
   }
 
   render() {
@@ -21,14 +87,19 @@ class UpdateProjectTask extends Component {
                   Back to Project Board
                 </a>
                 <h4 className="display-4 text-center">Update Project Task</h4>
-                <p className="lead text-center">Project Name + Project Code</p>
-                <form>
+                <p className="lead text-center">
+                  Project Name :{this.state.projectIdentifier} Project Task ID:
+                  {this.state.projectSequence}
+                </p>
+                <form onSubmit={this.onSubmit}>
                   <div className="form-group">
                     <input
                       type="text"
                       className="form-control form-control-lg"
                       name="summary"
                       placeholder="Project Task summary"
+                      value={this.state.summary}
+                      onChange={this.onChange}
                     />
                   </div>
                   <div className="form-group">
@@ -36,6 +107,8 @@ class UpdateProjectTask extends Component {
                       className="form-control form-control-lg"
                       placeholder="Acceptance Criteria"
                       name="acceptanceCriteria"
+                      value={this.state.acceptanceCriteria}
+                      onChange={this.onChange}
                     />
                   </div>
                   <h6>Due Date</h6>
@@ -44,12 +117,16 @@ class UpdateProjectTask extends Component {
                       type="date"
                       className="form-control form-control-lg"
                       name="dueDate"
+                      value={this.state.dueDate}
+                      onChange={this.onChange}
                     />
                   </div>
                   <div className="form-group">
                     <select
                       className="form-control form-control-lg"
                       name="priority"
+                      value={this.state.priority}
+                      onChange={this.onChange}
                     >
                       <option value={0}>Select Priority</option>
                       <option value={1}>High</option>
@@ -62,6 +139,8 @@ class UpdateProjectTask extends Component {
                     <select
                       className="form-control form-control-lg"
                       name="status"
+                      value={this.state.status}
+                      onChange={this.onChange}
                     >
                       <option value="">Select Status</option>
                       <option value="TO_DO">TO DO</option>
@@ -92,4 +171,4 @@ const mapStateToProps = (state) => ({
   project_task: state.backlog.project_task,
 });
 
-export default connect(null, { getProjectTask })(UpdateProjectTask);
+export default connect(mapStateToProps, { getProjectTask })(UpdateProjectTask);
