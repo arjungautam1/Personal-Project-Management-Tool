2236b9d402ff4381279f82eb0ad09ac4bbe72059
arjun-sudo <arjungautam5431@gmail.com>
2021-01-28 11:57:44 +0545

    spring security configuration

---
+++ src/main/java/codes/laser/ppmtool/controller/UserController.java
@@ -0,0 +1,10 @@
+/**
+ * @author Arjun Gautam
+ * Project :ppmtool
+ * Date : 2021-01-28
+ * Time : 11:48
+ */
+package codes.laser.ppmtool.controller;
+
+public class UserController {
+}
---
+++ src/main/java/codes/laser/ppmtool/model/User.java
@@ -0,0 +1,10 @@
+/**
+ * @author Arjun Gautam
+ * Project :ppmtool
+ * Date : 2021-01-28
+ * Time : 11:49
+ */
+package codes.laser.ppmtool.model;
+
+public class User {
+}
