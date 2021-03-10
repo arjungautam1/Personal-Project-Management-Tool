8be221ba65a53369327d14d21881344f2a47d8c8
arjun-sudo <arjungautam5431@gmail.com>
2021-03-10 20:33:21 +0545

    merge user registration

--- src/main/java/codes/laser/ppmtool/exceptions/UserNameAlreadyExistResponse.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author : Arjun Gautam
- * e-mail : arjungautam5431@gmail.com
- * Date :2021-03-10
- * Time :19:30
- */
-package codes.laser.ppmtool.exceptions;public class UserNameAlreadyExistResponse {
-}
