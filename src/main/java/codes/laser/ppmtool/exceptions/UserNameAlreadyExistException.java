596a7901b89059a9c0df8ffc7f77296e37577858
arjun-sudo <arjungautam5431@gmail.com>
2021-03-10 19:40:01 +0545

    User name already exist exception

---
+++ src/main/java/codes/laser/ppmtool/exceptions/UserNameAlreadyExistException.java
@@ -0,0 +1,8 @@
+/**
+ * @author : Arjun Gautam
+ * e-mail : arjungautam5431@gmail.com
+ * Date :2021-03-10
+ * Time :19:31
+ */
+package codes.laser.ppmtool.exceptions;public class UserNameAlreadyExistException {
+}
