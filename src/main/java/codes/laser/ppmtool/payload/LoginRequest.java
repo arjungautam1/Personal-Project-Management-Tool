a55098cbc6afbee294a6104667f8ad8884ecc044
arjun-sudo <arjungautam5431@gmail.com>
2021-03-11 07:41:24 +0545

    JWT config

---
+++ src/main/java/codes/laser/ppmtool/payload/LoginRequest.java
@@ -0,0 +1,33 @@
+/**
+ * @author : Arjun Gautam
+ * e-mail : arjungautam5431@gmail.com
+ * Date :2021-03-11
+ * Time :07:28
+ */
+package codes.laser.ppmtool.payload;
+
+import javax.validation.constraints.NotBlank;
+
+public class LoginRequest {
+
+    @NotBlank(message = "Username cannot be blank")
+    private String username;
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
