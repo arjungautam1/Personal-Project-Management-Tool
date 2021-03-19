393b480c204d0bcc683bc56a0f78a77e6c5d6778
arjun-sudo <arjungautam5431@gmail.com>
2021-03-19 21:29:26 +0545

    user specific update

--- src/main/java/codes/laser/ppmtool/services/ProjectService.java
+++ src/main/java/codes/laser/ppmtool/services/ProjectService.java
@@ -30,6 +30,16 @@ public class ProjectService {
     private UserRepository userRepository;
 
     public Project saveOrUpdateProject(Project project, String username) {
+
+        if (project.getId() != null) {
+            Project existingProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
+            if (existingProject != null && (!existingProject.getProjectLeader().equals(username))) {
+                throw new ProjectNotFoundException("Project Not found in your account");
+            } else if (existingProject == null) {
+                throw new ProjectNotFoundException("Project with ID :'" + project.getProjectIdentifier()+"'cannot be updated because it doesn't exist");
+            }
+        }
+
         try {
 
             User user = userRepository.findByUsername(username);
