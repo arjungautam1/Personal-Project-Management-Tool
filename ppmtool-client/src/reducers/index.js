7da78e19b7075b680e1150129948d72725edfc6e
arjun-sudo <arjungautam5431@gmail.com>
2021-03-23 07:38:24 +0545

    login completed

--- ppmtool-client/src/reducers/index.js
+++ ppmtool-client/src/reducers/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from "redux";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
 import backlogReducer from "./backlogReducer";
+import securityReducer from "./securityReducer";
 
 export default combineReducers({
     errors: errorReducer,
     project: projectReducer,
-    backlog: backlogReducer
+    backlog: backlogReducer,
+    security:securityReducer
 
 });
