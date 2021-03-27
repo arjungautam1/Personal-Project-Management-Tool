7053027ab48638c950ff1c39045837244adeaa2b
arjun-sudo <arjungautam5431@gmail.com>
2021-03-27 09:23:20 +0545

    dynamic header based on security state

--- ppmtool-client/src/components/Layout/Header.js
+++ ppmtool-client/src/components/Layout/Header.js
@@ -1,51 +1,104 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import PropTypes from "prop-types"
+import {connect} from "react-redux"
+import {logout} from "../../actions/securityActions";
+
+
 class Header extends Component {
-  render() {
-    return (
-      //Navbar Component
-      <div>
-        <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
-          <div className="container">
-            <Link className="navbar-brand" to="/dashboard">
-              Personal Project Management Tool
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#mobile-nav"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
 
+    logout() {
+        this.props.logout();
+        window.location.href = "/";
+    }
+
+    render() {
+
+        const {validToken, user} = this.props.security
+
+        const userIsAuthenticated = (
             <div className="collapse navbar-collapse" id="mobile-nav">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/dashboard">
-                    Dashboard
-                  </Link>
-                </li>
-              </ul>
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/dashboard">
+                            Dashboard
+                        </Link>
+                    </li>
+                </ul>
 
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="btn btn-lg btn-primary mr-2 " to={"/register"}>
-                    Sign Up
-                  </Link>
-                </li>
-                <li>
-                <Link className="btn btn-lg btn-primary mr-2 " to={"/login"}>
-                  Log in
-                </Link>
-                </li>
-              </ul>
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link className="btn btn-lg btn-primary mr-2 " to={"/dashboard"}>
+                            <i className="fas fa-user-circle mr-1"/>{user.fullName}
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className="btn btn-lg btn-primary mr-2 " to={"/logout"}  onClick={this.logout.bind(this)}>
+                            Logout
+
+                        </Link>
+                    </li>
+                </ul>
             </div>
-          </div>
-        </nav>
-      </div>
-    );
-  }
+        );
+        const userIsNotAuthenticated = (
+            <div className="collapse navbar-collapse" id="mobile-nav">
+
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link className="btn btn-lg btn-primary mr-2 " to={"/register"}>
+                            Sign Up
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className="btn btn-lg btn-primary mr-2 " to={"/login"} >
+                            Login
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        );
+
+        let headerLinks;
+        if (validToken && user) {
+            headerLinks = userIsAuthenticated
+        } else {
+            headerLinks = userIsNotAuthenticated
+        }
+
+
+        return (
+            //Navbar Component
+            <div>
+                <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+                    <div className="container">
+                        <Link className="navbar-brand" to="/dashboard">
+                            Personal Project Management Tool
+                        </Link>
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#mobile-nav"
+                        >
+                            <span className="navbar-toggler-icon"/>
+                        </button>
+                        {headerLinks}
+
+                    </div>
+                </nav>
+            </div>
+        );
+    }
 }
 
-export default Header;
+Header.propTypes = {
+    logout: PropTypes.func.isRequired,
+    security: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    security: state.security
+});
+
+export default connect(mapStateToProps, {logout})(Header);
